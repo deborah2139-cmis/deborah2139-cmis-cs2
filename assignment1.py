12a45c41515b9b5a481269661faf8c75604b8dc4
cmis <cmis@cmis-hs10.(none)>
2016-02-01 13:31:00 +0700

    Added changes to assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,18 @@
+myName = "Deborah"
+print myName
+myAge = 14.42
+print myAge
+myHeight = 1.62
+print myHeight
+LengthofSquare = 3
+print LengthofSquare
+LengthofRectangle = 5
+print LengthofRectangle
+HeightofRectangle = 3
+print HeightofRectangle
+myAgeinMonth = myAge * 12
+print myAgeinMonth
+YearslefttoLive = 80 - myAge
+print YearslefttoLive
+myHeightinFeet = myHeight * 3.28
+print myHeightinFeet
