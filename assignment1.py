21179bc7531069374ce0aa5b201299bdc5fcce9f
cmis <cmis@cmis-hs10.(none)>
2016-02-03 08:40:39 +0700

    Added changes to assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,18 +1,40 @@
 myName = "Deborah"
+#It tells what my name is
 print myName
 myAge = 14.42
+#It tells my age
 print myAge
 myHeight = 1.62
+#It tells how tall I am
 print myHeight
 LengthofSquare = 3
+#It tells the length of the square
 print LengthofSquare
 LengthofRectangle = 5
+#It tells the length of the rectangle
 print LengthofRectangle
 HeightofRectangle = 3
+#It tells the height of the rectangle
 print HeightofRectangle
 myAgeinMonth = myAge * 12
+#It tells what my age in month is
 print myAgeinMonth
 YearslefttoLive = 80 - myAge
+#It tells about how many years I have left to live
 print YearslefttoLive
 myHeightinFeet = myHeight * 3.28
+#It tells my height in feet
 print myHeightinFeet
+DifferenceofmyHeightandtheAverageHeightofKoreanGirl = 1.62 - myHeight
+#It tells the difference of the average height of Korean girls and my height
+print DifferenceofmyHeightandtheAverageHeightofKoreanGirl
+AreaofSquare = LengthofSquare * LengthofSquare
+print AreaofSquare
+HalfVolumeofCube = LengthofSquare * LengthofSquare * LengthofSquare / 2
+print HalfVolumeofCube
+CalculationofRectangle = LengthofRectangle * HeightofRectangle * 1.1
+print CalculationofRectangle
+print "Hello. My name is " + str(myName) + ". I am " + str(myAge) + " years old, which is about " + str(myAgeinMonth) + " months. I have about " + str(YearslefttoLive) + " more years left to live. My height is about " + str(myHeight) + " meters."
+print "Hello. My name is", myName, ". I am", myAge, " years old, which is about",  myAgeinMonth, " months. I have about", YearslefttoLive, "more years left to live. My height is about", myHeight, "meters."
+WinkingFace = ";)"
+print WinkingFace * 10000
