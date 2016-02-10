169eb62a21153eb8ea7213aa412ef5958c19311f
cmis <cmis@cmis-hs10.(none)>
2016-02-10 10:55:25 +0700

    Added changes to functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,26 @@
+import math
 def add(a, b):
 	return a + b
-add(3, 4)
-
+print add(3, 4)
+#Add
 def sub(a, b):
 	return a - b
-sub(5, 3)
+print sub(5, 3)
+
+def mul(a, b):
+	return a * b
+print mul(4, 4)
+
+def div(a, b):
+	return a/b
+print div(2, 3.0)
+
+def hours(a, b):
+	return a/b
+print div(86400, 3600)
+
+def mul(a, b, c):
+	return a * b * c
+print mul(3.14159265359, 5, 5)
+
+def 
