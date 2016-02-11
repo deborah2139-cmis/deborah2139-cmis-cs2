fd8977e283f0f5aca1d39dc84b2f496fbde2dad0
cmis <cmis@cmis-hs10.(none)>
2016-02-11 13:25:33 +0700

    Added changes to functions.py

--- functions.py
+++ functions.py
@@ -2,25 +2,39 @@ import math
 def add(a, b):
 	return a + b
 print add(3, 4)
-#Add
+#Adds 3 and 4
+
 def sub(a, b):
 	return a - b
 print sub(5, 3)
+#Subtracts 5 and 3
 
 def mul(a, b):
 	return a * b
 print mul(4, 4)
+#Multiplies 4 and 4
 
 def div(a, b):
 	return a/b
 print div(2, 3.0)
+#Divide 2 and 3.0
 
 def hours(a, b):
 	return a/b
 print div(86400, 3600)
+#Found the number of hours in 86400 seconds
 
-def mul(a, b, c):
-	return a * b * c
-print mul(3.14159265359, 5, 5)
+def area(a):
+	return math.pi* a **2
+print area(5)
+#Found the area of a circle
 
-def 
+def volume(v):
+	return (((4/3.0)*math.pi)*v**3)
+print volume(5)
+#Found the volume of the sphere
+#didn't do 2 questions yet
+
+def right_align(intro):
+""                                                                                "
+right_allign("Hello")
