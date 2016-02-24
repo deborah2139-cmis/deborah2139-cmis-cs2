e5626c74fbd8f2366563344e804c8d151ffefc5c
cmis <cmis@cmis-hs10.(none)>
2016-02-24 09:45:11 +0700

    Added changes to functions.py

--- functions.py
+++ functions.py
@@ -1,65 +1,78 @@
 import math
 
-def add (a, b):
+def add(a, b):
 	return a + b
 a = add(3, 4)
+#Adds 3 and 4
 
-def sub (d, e):
+def sub(d, e):
 	return d - e
 b = sub(5, 3)
+#Subtracts 5 and 3
 
-def mul (g, h):
+def mul(g, h):
 	return g * h
-c= mul (4, 4)
+c= mul(4, 4)
+#Multiplies 4 and 4
 
-def div (j, k):
+def div(j, k):
 	return (j / k)
-d= div (2.0, 3.0)
+d= div(2.0, 3.0)
+#Divides 2.0 and 3.0
 
-def hours_from_seconds (a):
+def hours_from_seconds(a):
 	return a / 3600
 print hours_from_seconds (86400)
 h= hours_from_seconds (86400)
+#Finds the hours in 86400 seconds
 
-def circle_area (a):
+def circle_area(a):
 	return math.pi * (a**2)
-print circle_area (5)
-r= circle_area (5)
+print circle_area(5)
+r= circle_area(5)
+#It calculates the area of the circle
 
-def sphere_volume (a):
+def sphere_volume(a):
 	return 1.33333333333 * math.pi * (a**3)
-print sphere_volume (5)
-v = sphere_volume (5)
+print sphere_volume(5)
+v = sphere_volume(5)
+#It calculates the volume of the sphere
 
-def avg_volume (a, b):
+def avg_volume(a, b):
 	 return ((1.0/6 * math.pi * a**3) + (1.0/6 * math.pi * b**3)) /2
 print avg_volume(10, 20)
-g = avg_volume (10, 20)
+g = avg_volume(10, 20)
+#It calculates the average volume
 
-def area (a, b, c):
+def area(a, b, c):
 	return math.sqrt (2.75*(2.75-a)*(2.75-b)*(2.75-c))
-print area (1.0, 2.0, 2.5)
-e = area (1.0, 2.0, 2.5)
+print area(1.0, 2.0, 2.5)
+e = area(1.0, 2.0, 2.5)
+#It calculates area of a triangle
 
-def right_align (word):
+def right_align(word):
 	return (80-len(word))*(" ")+word
-print right_align ("Hello")
-j= right_align ("Hello")
+print right_align("Hello")
+j= right_align("Hello")
+#It returns you "Hello" with 80 spaces in the front
 
-def center (word):
+def center(word):
 	return (40-len(word))*(" ")+word
-print center ("Hello")
-k= center ("Hello")
+print center("Hello")
+k= center("Hello")
+#It returns you "Hello", in the middle of 80 spaces
 
-def msg_box (word):
+def msg_box(word):
     return "+" + ((len(word)+ 4)*"-") + "+" + "\n" + "|" + (2*" ") + (word) + (2*" ") + "|" + "\n" + "+" + ((len(word) +4)*"-") + "+"
-print msg_box ("Hello")
-m = msg_box ("Hello")
+print msg_box("Hello")
+m = msg_box("Hello")
+#It returns you "Hello" in a box
 
-def msg_box (word):
+def msg_box(word):
     return "+" + ((len(word)+ 4)*"-") + "+" + "\n" + "|" + (2*" ") + (word) + (2*" ") + "|" + "\n" + "+" + ((len(word) +4)*"-") + "+"
-print msg_box ("I eat cats!")
-n = msg_box ("I eat cats!")
+print msg_box("I eat cats!")
+n = msg_box("I eat cats!")
+#It returns you "I eat cats!" in a box
 
 print msg_box(str(a))
 print msg_box(str(b))
