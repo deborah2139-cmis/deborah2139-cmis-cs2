4d4dee3e7de386a9a8523e292f91736916a5fd37
cmis <cmis@cmis-hs10.(none)>
2016-03-01 10:55:32 +0700

    Added changes to borac.py

---
+++ borac.py
@@ -0,0 +1,49 @@
+def jumpAround():
+	print "jump jump"
+def hopAround():
+	print "hop hop"
+def runAround():
+	print "wheee"
+youAreExited = True
+youFeelLikeDoingSomething = True
+
+if youAreExited and youFeelLikeDoingSomething:
+	jumpAround()
+	hopAround()
+	runAround()
+
+print "Ouch! You fell down!!!"
+
+def cry():
+	print "sob sob"
+def scream():
+	print "scream scream"
+
+youAreSad = True
+youWantToBuySomething = True
+
+if youAreSad and youWantToBuySomething:
+	cry()
+	scream()
+
+print "Are you okay?"
+
+def standUp():
+	print "heeyahh!!"
+def stompYourFeet():
+	print "stomp stomp"
+def wipeYourTears():
+	print "wipe wipe"
+def sayIamOkay():
+	print "I am okayy!!!"
+
+youAreInjured = True
+itsAllFine = True
+
+if youAreInjured and itsAllFine:
+	standUp()
+	stompYourFeet()
+	wipeYourTears()
+	sayIamOkay
+
+print "I'm all FIne!!!"
