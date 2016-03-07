ef2ed7a8202395e5e4a508347bb5bbd5c94d844b
cmis <cmis@cmis-hs10.(none)>
2016-03-07 10:56:21 +0700

    Added changes to simple.py

--- simple.py
+++ simple.py
@@ -1,2 +1,20 @@
-def output():
-	return 
+import math
+
+def mult(m, v):
+	return m * v
+
+def output(m, v, P):
+	return """
+The momentum of first ball:
+{} * {} = {}
+""". format(m, v, P)
+
+
+def main():
+	mass1 = raw_input("Mass of a ball: ")
+	velocity1 = raw_input("Velocity of a ball: ")
+	mass2 = raw_input("Mass of second ball: ")
+	velocity2 = raw_input("Velocity of second ball: ")
+	
+
+main()
