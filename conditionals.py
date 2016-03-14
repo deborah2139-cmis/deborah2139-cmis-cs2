f57976a06c08da021695dae1918ad616a8d69bbd
cmis <cmis@cmis-hs10.(none)>
2016-03-14 10:55:27 +0700

    Added changes to conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,13 @@
+def minimumno():
+	number = random.randint
+	return minimumno
+
+
+
+def main():
+	minimumno = raw_input("What is the minimum number?: ")
+	maximumno = raw_input("What is the maximum number?: ")
+	print "I'm thinking of a number fromto"
+	you = raw_input("What do you think it is?: ")
+
+main()
