1c3d23cea9acbe781621d6912cadccb6a33917df
cmis <cmis@cmis-hs19.(none)>
2016-03-17 08:40:38 +0700

    Added changes to conditionals.py

--- conditionals.py
+++ conditionals.py
@@ -1,14 +1,16 @@
-def minimumno():
-	number = random.random()
-	return minimumno
+import math 
+import random
 
 
+def numberchoice(minimumno, maximumno):
+	return random.randint(minimumno, maximumno)
+
+def chooserchoice
 
 def main():
 	minimumno = raw_input("What is the minimum number?: ")
 	maximumno = raw_input("What is the maximum number?: ")
-	print "I'm thinking of a number from"
-	
+	print "I'm thinking of a number from " + str(minimumno) + " to " + str(maximumno) + "."
 	you = raw_input("What do you think it is?: ")
 
 main()
