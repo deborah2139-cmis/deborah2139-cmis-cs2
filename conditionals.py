743c24e3688509324fe25170872a8e8523c3b3d1
cmis <cmis@cmis-hs19.(none)>
2016-03-21 10:44:29 +0700

    Added changes to conditionals.py

--- conditionals.py
+++ conditionals.py
@@ -1,16 +1,33 @@
-import math 
 import random
 
-
-def numberchoice(minimumno, maximumno):
+def numberChoice(minimumno, maximumno):
 	return random.randint(minimumno, maximumno)
 
-def chooserchoice
+def guesserGuess(myChoice, playerGuess):
+	if myChoice < playerGuess:
+		dif = abs(playerGuess - myChoice)
+		return "Your guess is over by {}".format(dif)
+	if myChoice > playerGuess:
+		dif = abs(myChoice - playerGuess)
+		return "Your guess is under by {}".format(dif)
+
+def output(myChoice, playerGuess, incorrect):
+	if myChoice == playerGuess:
+		print """The target was {}
+Your guess was {}
+That is correct! Are you a mind(computer) reader?!""".format(myChoice, playerGuess)
+	else:
+		print """The target was {}
+Your guess was {}
+{}""".format(myChoice, playerGuess, incorrect)
 
 def main():
-	minimumno = raw_input("What is the minimum number?: ")
-	maximumno = raw_input("What is the maximum number?: ")
-	print "I'm thinking of a number from " + str(minimumno) + " to " + str(maximumno) + "."
-	you = raw_input("What do you think it is?: ")
+	minimumno = int(raw_input("What is the minimum number?: "))
+	maximumno = int(raw_input("What is the maximum number?: "))
+	print "I'm thinking of a number from {} to {}".format(minimumno, maximumno)
+	playerGuess = int(raw_input("What do you think it is?: "))
+	myChoice = int(numberChoice(minimumno, maximumno))
+	incorrect = guesserGuess(myChoice,playerGuess)
+	return output(myChoice, playerGuess,incorrect)
 
 main()
