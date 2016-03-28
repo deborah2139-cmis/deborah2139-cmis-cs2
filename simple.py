d961d5de3a773a18d70a179a803f7f712d84bfca
cmis <cmis@cmis-hs19.(none)>
2016-03-28 10:46:02 +0700

    Added changes to simple.py

--- simple.py
+++ simple.py
@@ -1,29 +1,26 @@
 import math
 
-def momentum1(m, v):
-	return """The momentum of first ball:
-{} * {} = {}""".format(m, v, P)
+def momentum1(mass1, velocity1):
+	return mass1 * velocity1
 
-def momentum2(m2, v2):
-	return m2 * v2
+def momentum2(mass2, velocity2):
+	return mass2 * velocity2
 
-def output(m, v, P):
-	return """
+def output(mass1, velocity1, P1, mass2, velocity2, P2):
+	print """
 The momentum of first ball:
 {} * {} = {}
-""".format(m, v, P)
-	return """
-the momentum of second ball:
+The momentum of second ball:
 {} * {} = {}
-""".format(m2, v2, P2)
-
-
+""".format(mass1, velocity1, P1, mass2, velocity2, P2)
+	
 def main():
-	mass1 = raw_input("Mass of a ball: ")
-	velocity1 = raw_input("Velocity of a ball: ")
-	P = momentum1(m, v)
-	mass2 = raw_input("Mass of second ball: ")
-	velocity2 = raw_input("Velocity of second ball: ")
-	P2 = momentum2(m2, v2)
-	return output(m, v, P)
+	mass1 = int(raw_input("Mass of a ball: "))
+	velocity1 = int(raw_input("Velocity of a ball: "))
+	mass2 = int(raw_input("Mass of second ball: "))
+	velocity2 = int(raw_input("Velocity of second ball: "))
+	P1 = int(momentum1(mass1, velocity1))
+	P2 = int(momentum2(mass2, velocity2))
+	return output(mass1, velocity1, P1, mass2, velocity2, P2)
+
 main()
