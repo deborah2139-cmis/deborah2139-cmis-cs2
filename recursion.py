5c8d07aa5ec13988831110b632dcf47c89cc017d
cmis <cmis@cmis-hs19.(none)>
2016-04-22 13:35:23 +0700

    Added changes to recursion.py

--- recursion.py
+++ recursion.py
@@ -1,3 +1,10 @@
+def countup(n):
+	if n >= 11:
+		print "Blastoff!"
+	else:
+		print n
+		countup(n+1)
+
 def countdown(n):
 	if n <= 0:
 		print "Blastoff!"
@@ -5,16 +12,28 @@ def countdown(n):
 		print n
 		countdown(n-1)
 
-def countup(n):
-	if n>= 0:
+def countup_from_to(start, stop):
+	if start >= stop:
 		print "Blastoff!"
 	else:
-		print n
+		print start
+		countup(start+1)
 
+def countdown_from_to(start, stop):
+	if start <= stop:
+		print "Blastoff!"
+	else:
+		print start
+		countdown(start-1)
+
+def adder():
+	pass
 
 def main():
-	countup(-10)
-	countup(0)
-	countup(10)
+	countup(1)
+	countdown(10)
+	countup_from_to(20, 34)
+	countdown_from_to(30, 12)
+	
 
 main()
