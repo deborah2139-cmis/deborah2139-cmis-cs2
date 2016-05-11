bf76ebcc2232351d1d6a26268e7b61ac679c9e28
cmis <cmis@cmis-hs19.(none)>
2016-05-11 13:34:59 +0700

    Added changes to conditionals.py

--- conditionals.py
+++ conditionals.py
@@ -9,28 +9,36 @@ import random
 def shoes(choices):
 	if choices == "flip flops":
 		print "You chose flip flops and got kicked out of the shop! [^,^]"
+
 	elif choices == "tennis shoes":
 		print "What a nice choice! You get to work in the farm for 7 days starting from tomorrow! xD"
+
 	elif choices == "sandals":
 		print"Get $400 :D"
 	else:
+
 		print "You only have 3 choices :( Bye :("
 
 def free(gender):
 	if gender == "female":
 		print "You get pants for males for free"
+
 	elif gender != "female":
 		print "You get a super short mini skirt for free"
+
 	else:
 		print "You gotta be a male or a female... X( Bye"
 
 def pants(kind):
 	if kind == "long denim pants":
 		print "I'm sorry, you need to hike in those :P"
+
 	elif kind == "short pants":
 		print "Wise choice :)"
+
 	elif kind == "skirts":
 		print "I'll just assume that you are a female ;)"
+
 	else:
 		print "NOOOO YOU ONLY HAVE THREE CHOICES! BYEE!"
 
@@ -41,17 +49,30 @@ def thechosenone(mynumb, chosennumb):
 	if mynumb < chosennumb:
 		dif = abs(chosennumb - mynumb)
 		return "Your number is over by {}".format(dif)
+
 	if mynumb > chosennumb:
 		dif = abs(mynumb - chosennumb)
 		return "Your number is under by {}".format(dif)
 
 def hat(types):
-	snapback = True
-	
+	if types == "cowboyhat":
+		return True
+
+	else:
+		return False
+
+def whathat(types):
+	if types == True:
+		msg = "You are very lucky!! You get $1000"
+	else:
+		msg = "Wrong!! I'm sorry but there's no prize for a loser :("
 
 def output(mynumb, chosennumb, wrong):
 	if mynumb == chosennumb:
 		print """That was right!!! The number was {}! You get 3 jackets in return!! :D""".format(mynumb)
+	elif chosennumb > mynumb:
+		return """NOOO!!!! YOUR MAXIMUM NUMBER IS {}!!!""".format(chosennumb)
+
 	else:
 		print """Your guess, {}, was not right!! The number was {}. I'm sorry, but there's no second chance :( {}""".format(chosennumb, mynumb, wrong)
 	 
@@ -64,11 +85,14 @@ def main():
 	undclothes = pants(kind)
 	number1 = int(raw_input("Type any number: "))
 	number2 = int(raw_input("Type another number: "))
+	types = raw_input("You need to choose one hat! YOU NEED TO CHOOSE THE RIGHT ONE: snapback, beanie, or cowboyhat: ")
+	hatt = hat(types)
+	whathatt = whathat(types)
 	print "I'm thinking of a number from {} to {}. If you guess the number right, you get 3 different jackets.".format(number1, number2)
 	chosennumb = int(raw_input("Take a guess!!: "))
 	mynumb = int(jacket(number1, number2))
 	wrong = thechosenone(mynumb, chosennumb)
 	return output(mynumb, chosennumb, wrong)
-	print "You need to choose one hat. If you choose the wrong one, there will be a penalty..."
+	
 
 main()
