6013d70072b45c655f6780fc82c7f72308bf1b51
cmis <cmis@cmis-hs02.(none)>
2016-05-13 15:04:49 +0700

    Added changes to conditionals.py

--- conditionals.py
+++ conditionals.py
@@ -1,7 +1,6 @@
 #My script will let you make choices about what to buy. It will sometimes make you do some work like hiking unless you are lucky and you make a wise choice
 
 #At least 1 must return a boolean value and be used as a part of the flow control.
-#Use "not" "or" "and"
 #Use random.random()
 
 import random
@@ -42,24 +41,29 @@ def pants(kind):
 	else:
 		print "NOOOO YOU ONLY HAVE THREE CHOICES! BYEE!"
 
-#def whathat(whattype):
-	#if whattype == True:
-	#	msg = "You are very lucky!! You get $1000"
-	#else:
-	#	msg = "Wrong!! I'm sorry but there's no prize for a loser :("
-	#return """
-#{}""".format(msg)
+def whathat(whattype):
+	if whattype == True:
+		msg = "You are very lucky!! You get $1000"
+	else:
+		msg = "Wrong!! I'm sorry but there's no prize for a loser :("
+	return """
+{}""".format(msg)
 
 def jacket(number1, number2):
 	return random.randint(number1, number2)
 
 def thechosenone(mynumb, chosennumb):
-	if mynumb < chosennumb:
+	if mynumb < chosennumb and not mynumb == chosennumb:
 		dif = abs(chosennumb - mynumb)
 		return "Your number is over by {}".format(dif)
-	if mynumb > chosennumb:
+	if mynumb > chosennumb or not mynumb < chose:
 		dif = abs(mynumb - chosennumb)
 		return "Your number is under by {}".format(dif)
+
+def yourmoneyleft():
+	yourmoney = int(random.random) * 1000
+	return int(yourmoney)
+#wrong?
 
 def output(mynumb, chosennumb, wrong):
 	if mynumb == chosennumb:
@@ -76,13 +80,15 @@ def main():
 	femormale = free(gender)
 	kind = raw_input("Choose one of these long denim pants, short pants, skirts: ")
 	undclothes = pants(kind)
-	#whattype = raw_input("You need to choose one hat! YOU NEED TO CHOOSE THE RIGHT ONE: snapback, beanie, or cowboyhat: ")
-	#whathatt = whathat(whattype)
+	whattype = raw_input("You need to choose one hat! YOU NEED TO CHOOSE THE RIGHT ONE: snapback, beanie, or cowboyhat: ")
+	whathatt = whathat(whattype)
 	number1 = int(raw_input("Type any number: "))
 	number2 = int(raw_input("Type another number: "))
 	print "I'm thinking of a number from {} to {}. If you guess the number right, you get 3 different jackets.".format(number1, number2)
 	chosennumb = int(raw_input("Take a guess!!: "))
+	#do the raw input thing for yourmoney
 	mynumb = int(jacket(number1, number2))
+	yourmoney = yourmoneyleft()
 	wrong = thechosenone(mynumb, chosennumb)
 	return output(mynumb, chosennumb, wrong)
 
