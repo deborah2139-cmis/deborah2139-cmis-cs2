40032cef120db8b43d3204b97250bcee16ec0c76
cmis <cmis@cmis-hs19.(none)>
2016-05-26 08:43:27 +0700

    looping stuff

---
+++ term.py
@@ -0,0 +1,22 @@
+def countFromZ(x, y):
+	if x > y:
+		while x >= y:
+			print x
+			x -= 1
+
+	if x < y:
+		while x <= y:
+			print y
+			y -= 1
+
+
+def addOdds(n):
+	if n > 0:
+		while n > 0:
+
+
+def main():
+	countFromZ(7, 2)
+	addOdds(19)
+
+main()
