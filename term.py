68a60aaaba1ac6e752da1e0a61dc15220d4d8c48
cmis <cmis@cmis-hs19.(none)>
2016-05-27 10:56:04 +0700

    Looping example

--- term.py
+++ term.py
@@ -4,18 +4,38 @@ def countFromZ(x, y):
 			print x
 			x -= 1
 
-	if x < y:
+	elif x < y:
 		while x <= y:
 			print y
 			y -= 1
 
 
 def addOdds(n):
+	sum = 0
 	if n > 0:
 		while n > 0:
+			if n & 2 == 1:
+				sum += 1
+			n -=1
 
+	if n < 0:
+		while n < 0:
+			if n & 2 == 1:
+				sum += 1
+			n += 1
+	return sum
+
+
+def grid(w, h):
+	out = ""
+	x = 0
+	while x < w:
+		out += "." * w
+		x += 1
+	return out
 
 def main():
+	print grid(6, 2)
 	countFromZ(7, 2)
 	addOdds(19)
 
