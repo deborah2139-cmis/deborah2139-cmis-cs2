b5114a747552274bfbe978eccdb7f79de97df538
cmis <cmis@cmis-hs07.(none)>
2016-06-01 14:04:42 +0700

    THE FINAL ONEEEEE

--- recursion.py
+++ recursion.py
@@ -1,17 +1,35 @@
-import math
+import random
+def game(compnumber, attempts):
+	yourguess = int(raw_input ("Guess de numberrr!!!!!: "))
+	if attempts == 0:
+		print "You're reeaaally bad at this you know"
+		return 0
+	elif int(yourguess) > int(compnumber):
+		print "That's too high!!!!!!!"
+		game(compnumber, attempts - 1)
+		return 0
+	elif int(yourguess) < int(compnumber):
+		print "That's too low!!!!!@!!!!"
+		game(compnumber, attempts - 1)
+		return 0
+	elif compnumber == yourguess:
+		print "Correctt"
+		return 1
 
-def candies(candy):
-	if candy < 0:
-		print "Candy Firework!!!!!!!!"
-	else:
-		print candy
-		candies(candy - 1)
-
-
-
-def main():
-	candy = raw_input("You're in a candy shop and we're counting down from 10 to 0... I promise, we're not suicide bombers :) Wait for it! Something sweet will happen! Type 10: ")
-	candies(10)
-
-
-main()
+def turns(roundd, correct, attempts):
+	compnumber = random.randint (0,100)
+	if roundd == 0:
+		print correct
+	elif roundd != 0:
+		computerthinks = ("I am thinking of a number between 0 and 100")
+		print computerthinks
+		thegame = game(compnumber, attempts)
+		adding = correct + thegame
+		print "You oonnlyy have {} rounds left".format(roundd - 1)
+		turns(roundd - 1, adding, attempts)
+def main(): 
+	correct = 0
+	attempts = 4
+	roundd = 3
+	turns(roundd, correct, attempts)
+main ()
